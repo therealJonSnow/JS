61c0b4b6f7df75b5a11ab15d551e42282ed98458
Jon Snow <47143928+therealJonSnow@users.noreply.github.com>
2023-08-16 08:48:39 +0100

    changes

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col w-full p-4 min-h-screen">
       <div class="flex items-center space-x-4">
         <!-- fixed top-0 left-16  -->
-        <TheLogo class="w-24 h-fit"/>
+        <TheLogo @click="$router.go('/')" class="w-24 h-fit"/>
         <div ref="titleBlock" class="relative overflow-hidden">
           <h1 class="text-5xl mt-4 opacity-0">//Blog</h1>
           <div class="absolute top-0 left-0 w-0 h-full bg-gray-800"></div>
@@ -18,6 +18,11 @@
             <!-- <NuxtLink :to="post._path" :data-before="post.title">{{ post.title }}</NuxtLink> -->
             <NuxtLink to="/blog" :data-before="post.title">{{ post.title }}
             </NuxtLink>
+            <svg viewBox="0 0 100 100">
+    <mask id="myMask"></mask>
+
+    <image class="image" x=0 y=0 width="100%" height="100%" href="https://assets.codepen.io/489403/grand_canyon.jpeg" preserveAspectRatio="xMidYMid slice" mask="url(#myMask)" />
+  </svg>
           </li>
         </ul>
       </main>
